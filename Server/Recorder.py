708b4df2e7fa80384becf21d236f3b03bc18dc6d
avitalll16 <avitalll16@gmail.com>
2022-05-20 01:29:51 +0300

    Reformat recorder

--- Server/Recorder.py
+++ Server/Recorder.py
@@ -1,26 +1,29 @@
+import logging
+
 import sounddevice
 import speech_recognition as sr
 from scipy.io.wavfile import write
 
+logger = logging.getLogger(__name__)
+
+
 def record(seconds: int) -> None:
     fs = 44100
-    print('Recording.....')
+    logger.debug('Recording.....')
     sounddevice.default.dtype = 'int32', 'int32'
     record_voice = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
     sounddevice.wait()
     write('../out.wav', fs, record_voice)
-    print('Finished.....Please check your output file')
+    logger.debug('Recording finished')
 
 
 def voice_to_text(voice_file_path: str) -> str:
-    r = sr.Recognizer()
+    recognizer = sr.Recognizer()
     audio_file = sr.AudioFile(voice_file_path)
-    with audio_file as source:
-        audio = r.record(source)
     try:
-        s = r.recognize_google(audio)
-        return s
-    except:
-        exception_msg = "error while trying to translate voice to tesxt from google"
-        # log_helper.write_excetion_log(exception_msg)
-        return exception_msg
+        with audio_file as source:
+            audio = recognizer.record(source)
+        text = recognizer.recognize_google(audio)
+        return text
+    except Exception as ex:
+        logger.exception(f'{ex}')
