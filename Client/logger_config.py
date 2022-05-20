c3bdd7fbbf5ba38012db50ada36957af8e5f4d22
avitalll16 <avitalll16@gmail.com>
2022-05-20 23:35:04 +0300

    Separate logger configurations

--- Client/logger_config.py
+++ Client/logger_config.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(filename=f'server_logs.log',
+logging.basicConfig(filename=f'client_logs.log',
                     filemode='a',
                     format='%(levelname)s %(asctime)s - %(message)s',
                     level=logging.DEBUG)
