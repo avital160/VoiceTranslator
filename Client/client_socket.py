f46aa2b95cc226c65e4ee4e5c73c1df6f66a171b
avitalll16 <avitalll16@gmail.com>
2022-05-20 23:42:24 +0300

    Add communication via sockets

--- Client/client_socket.py
+++ Client/client_socket.py
@@ -1,1 +1,50 @@
-# TODO send wav files to server
+import logging
+import os
+import socket
+
+import secrets
+
+logger = logging.getLogger(__name__)
+
+SERVER_ADDRESS = secrets.SERVER_ADDRESS
+PORT = secrets.SERVER_PORT
+
+
+def connect() -> socket.socket:
+    client_socket = None
+    try:
+        client_socket = socket.socket()
+        client_socket.connect((SERVER_ADDRESS, PORT))
+        logger.debug(f'connected successfully {client_socket=}')
+        return client_socket
+    except Exception as ex:
+        logger.exception(f'failed to connect to the server {ex=}')
+        if client_socket:
+            client_socket.close()
+
+
+def send_file_via_socket(client_socket: socket.socket, file_path: str, remove_file_after_sending: bool = True) -> None:
+    try:
+        with open(file_path, 'rb') as wav_file:
+            data = wav_file.read()
+
+        if remove_file_after_sending:
+            os.remove(file_path)
+
+        length = (str(len(data))).zfill(8)
+        client_socket.send(length.encode())
+
+        client_socket.send(data)
+
+        logger.debug(f'{file_path=} sent')
+    except Exception as ex:
+        logger.exception(f'{ex}')
+
+
+def connect_and_send_wav_file(wav_path):
+    client_socket = connect()
+    if not client_socket:
+        logger.info('client_socket is None')
+        return
+
+    send_file_via_socket(client_socket, wav_path)
--- Server/server_socket.py
+++ Server/server_socket.py
@@ -1,1 +1,43 @@
-# TODO get wav files from client
+import logging
+import socket
+import threading
+
+import secrets
+from utils import generate_random_filename
+
+logger = logging.getLogger(__name__)
+
+SERVER_ADDRESS = secrets.SERVER_ADDRESS
+PORT = secrets.SERVER_PORT
+
+
+def start_server() -> None:
+    server_socket = None
+    try:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((SERVER_ADDRESS, PORT))
+        server_socket.listen(5)
+        while True:
+            client_socket, address = server_socket.accept()
+            threading.Thread(target=conn_with_client, args=[client_socket]).start()
+
+    except:
+        if server_socket:
+            server_socket.close()
+
+
+def conn_with_client(client_socket: socket.socket) -> None:
+    try:
+        length = client_socket.recv(8)
+        length = int(length.decode())
+        data = client_socket.recv(length)
+
+        wav_path = generate_random_filename('wav')
+
+        with open(wav_path, 'wb') as wav_file:
+            wav_file.write(data)
+
+    except Exception as ex:
+        logger.exception(f'{ex}')
+        if client_socket:
+            client_socket.close()
