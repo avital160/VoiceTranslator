44a99f80134a0b0b2fb7f377328c0ba500477a50
avitalll16 <avitalll16@gmail.com>
2022-05-21 23:30:03 +0300

    rename "add_date" to "with_date" and add "with_file" attribute

--- Server/mail_format.py
+++ Server/mail_format.py
@@ -4,15 +4,17 @@ from abc import ABC, abstractmethod
 class Mail(ABC):
     sender: str
     contacts: tuple
-    add_date: bool
+    with_date: bool
+    with_file: bool
 
-    def __init__(self, sender: str, contacts: tuple, add_date: bool) -> None:
+    def __init__(self, sender: str, contacts: tuple, with_date: bool) -> None:
         self.sender = sender
         self.contacts = contacts
-        self.add_date = add_date
+        self.with_date = with_date
+        self.with_file = False
 
     def __str__(self):
-        return f'{self.sender=} {self.contacts=} {self.add_date=}'
+        return f'{self.sender=} {self.contacts=} {self.with_date=}'
 
     @abstractmethod
     def get_mail_subject(self):
@@ -26,12 +28,13 @@ class Mail(ABC):
 class FileMail(Mail):
     file_path: str
 
-    def __init__(self, sender: str, contacts: tuple, add_date: bool, file_path: str) -> None:
-        super().__init__(sender, contacts, add_date)
+    def __init__(self, sender: str, contacts: tuple, with_date: bool, file_path: str) -> None:
+        super().__init__(sender, contacts, with_date)
+        self.with_file = True
         self.file_path = file_path
 
     def __str__(self):
-        return f'{self.sender=} {self.contacts=} {self.add_date=} {self.file_path=}'
+        return f'{self.sender=} {self.contacts=} {self.with_date=} {self.file_path=}'
 
     def get_mail_subject(self):
         pass
@@ -43,12 +46,12 @@ class FileMail(Mail):
 class MessageMail(Mail):
     message: str
 
-    def __init__(self, sender: str, contacts: tuple, add_date: bool, message: str) -> None:
-        super().__init__(sender, contacts, add_date)
+    def __init__(self, sender: str, contacts: tuple, with_date: bool, message: str) -> None:
+        super().__init__(sender, contacts, with_date)
         self.message = message
 
     def __str__(self):
-        return f'{self.sender=} {self.contacts=} {self.add_date=} {self.message=}'
+        return f'{self.sender=} {self.contacts=} {self.with_date=} {self.message=}'
 
     def get_mail_subject(self):
         pass
@@ -60,16 +63,16 @@ class MessageMail(Mail):
 class TranslatedMessageMail(MessageMail):
     translation_language: str
 
-    def __init__(self, sender: str, contacts: tuple, add_date: bool, message: str, translation_language: str) -> None:
-        super().__init__(sender, contacts, add_date, message)
+    def __init__(self, sender: str, contacts: tuple, with_date: bool, message: str, translation_language: str) -> None:
+        super().__init__(sender, contacts, with_date, message)
         self.translation_language = translation_language
 
     def __str__(self):
-        return f'{self.sender=} {self.contacts=} {self.add_date=} {self.message=} {self.translation_language}'
+        return f'{self.sender=} {self.contacts=} {self.with_date=} {self.message=} {self.translation_language}'
 
 
 class SharedMessageMail(MessageMail):
-    def __init__(self, sender: str, add_date: bool, message: str) -> None:
+    def __init__(self, sender: str, with_date: bool, message: str) -> None:
         # TODO get all contracts method
         all_contracts = ()
-        super().__init__(sender, all_contracts, add_date, message)
+        super().__init__(sender, all_contracts, with_date, message)
