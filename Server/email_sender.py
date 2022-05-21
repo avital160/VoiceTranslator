803aad41d25c1f13e972e0390385d97e5f55f38e
avitalll16 <avitalll16@gmail.com>
2022-05-21 22:14:08 +0300

    Update email_sender.py

--- Server/email_sender.py
+++ Server/email_sender.py
@@ -7,31 +7,27 @@ from email.mime.text import MIMEText
 from pathlib import Path
 from secrets import EMAIL_USERNAME, EMAIL_ADDRESS, EMAIL_PASSWORD, EXAMPLE_CONTACTS
 
-SUBJECT = 'Voice Translator New Message!'
-BODY = 'Message Received: {}\n' \
-       'Translation in {}: {}'
 contacts = EXAMPLE_CONTACTS
 
 logger = logging.getLogger(__name__)
 
 
-def create_mail(receiver_address: str, msg: str, lang: str, translated: str, file_attachment_path: str = None) -> str:
+def create_mail(receiver_address: str, subject: str, body: str, file_attachment_path: str = '') -> str:
     try:
-        my_body = BODY.format(msg, lang.capitalize(), translated)
-
         mail_content = MIMEMultipart()
-        mail_content['Subject'] = SUBJECT
+        mail_content['Subject'] = subject
         mail_content['From'] = EMAIL_ADDRESS
         mail_content['To'] = receiver_address
 
-        mail_content.attach(MIMEText(my_body))
+        mail_content.attach(MIMEText(body))
 
         part = MIMEBase('application', "octet-stream")
-        with open(file_attachment_path, 'rb') as file:
-            part.set_payload(file.read())
-        encoders.encode_base64(part)
+        if file_attachment_path:
+            with open(file_attachment_path, 'rb') as file:
+                part.set_payload(file.read())
+                encoders.encode_base64(part)
 
-        part.add_header('Content-Disposition', 'attachment; filename={}'.format(Path(file_attachment_path).name))
+                part.add_header('Content-Disposition', f'attachment; filename={Path(file_attachment_path).name}')
         mail_content.attach(part)
 
         logger.debug('mail created')
@@ -41,14 +37,14 @@ def create_mail(receiver_address: str, msg: str, lang: str, translated: str, fil
         logging.exception(f'{ex}')
 
 
-def send_email(contact: str, msg: str, lang: str, translated: str) -> None:
+def send_email(subject: str, body: str, contact: str, msg: str, lang: str, translated: str) -> None:
     receiver_address = contacts.get(contact, '')
     print("receiver_address: " + receiver_address)
     if not receiver_address:
         logger.info(f'{receiver_address} was not found in contacts')
         return
 
-    mail_content = create_mail(receiver_address, msg, lang, translated)  # Create SMTP body
+    mail_content = create_mail(subject, body, receiver_address, msg)  # Create SMTP body
     if not mail_content:
         return
 
