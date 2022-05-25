0a38927a728cc1c014ce06d37e7c55e2331a5a6d
Avital <avitalll16@gmail.com>
2022-05-25 18:30:47 +0300

    to do and get mail object,body Changes by Doriav Isakov

--- Server/mail_format.py
+++ Server/mail_format.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from secrets import APP_CONTACTS
 
 
+# TODO get_correct_object should return the object of the current class and validate with check the correct format
 class Mail(ABC):
     sender: str
     contacts: tuple
@@ -17,11 +18,11 @@ class Mail(ABC):
         return f'{self.sender=} {self.contacts=} {self.with_date=}'
 
     @abstractmethod
-    def get_mail_subject(self):
+    def get_mail_subject(self) -> str:
         raise NotImplementedError
 
     @abstractmethod
-    def get_mail_body(self):
+    def get_mail_body(self) -> str:
         raise NotImplementedError
 
     def get_date_str_for_mail(self):
@@ -30,24 +31,13 @@ class Mail(ABC):
         else:
             return ''
 
-
-class FileMail(Mail):
-    file_path: str
-
-    def __init__(self, sender: str, contacts: tuple, with_date: bool, file_path: str) -> None:
-        super().__init__(sender, contacts, with_date)
-        self.file_path = file_path
-
-    def __str__(self):
-        return f'{self.sender=} {self.contacts=} {self.with_date=} {self.file_path=}'
-
-    def get_mail_subject(self):
-        return f'{self.sender} sent you a file via VoiceTranslator!'
-
-    def get_mail_body(self):
-        return self.get_date_str_for_mail()
+    @staticmethod
+    @abstractmethod
+    def get_correct_object(sender: str, text: str):
+        raise NotImplementedError
 
 
+# send {message} to {contact} with date (optional)
 class MessageMail(Mail):
     message: str
 
@@ -58,14 +48,31 @@ class MessageMail(Mail):
     def __str__(self):
         return f'{self.sender=} {self.contacts=} {self.with_date=} {self.message=}'
 
-    def get_mail_subject(self):
+    def get_mail_subject(self) -> str:
         return f'{self.sender} sent you a message via VoiceTranslator!'
 
-    def get_mail_body(self):
+    def get_mail_body(self) -> str:
         return f'{self.get_date_str_for_mail()}' \
                f'{self.message}'
 
+    @staticmethod
+    def get_correct_object(sender: str, text: str):
+        try:
+            assert text
+            words = text.split()
+            assert words[0] == 'send'
+            with_date = ' '.join(words[-2:]) == 'with date'
+            to_index = words.index('to')
+            contact = words[to_index + 1]
+            assert ' '.join(words[to_index + 1:]) not in['with date',""]
+            contacts = (contact,)
+            message = ' '.join(words[1:to_index])
+            return MessageMail(sender, contacts, with_date, message)
+        except:
+            return None
 
+
+# send {message} to {contact} in {language} with data (optional)
 class TranslatedMessageMail(MessageMail):
     translated_message: str
 
@@ -76,19 +83,80 @@ class TranslatedMessageMail(MessageMail):
     def __str__(self):
         return f'{self.sender=} {self.contacts=} {self.with_date=} {self.message=} {self.translated_message=}'
 
-    def get_mail_subject(self):
+    def get_mail_subject(self) -> str:
         return f'{self.sender} sent you a translated message via VoiceTranslator!'
 
-    def get_mail_body(self):
+    def get_mail_body(self) -> str:
         return f'{self.get_date_str_for_mail()}' \
                f'Original Message: {self.message}\n' \
                f'Translated Message: {self.translated_message}'
 
+    @staticmethod
+    def get_correct_object(sender: str, text: str):
+        #fixme here
+        try:
+            pass
+        except:
+            return None
 
+
+# send {message} to everybody with date
 class SharedMessageMail(MessageMail):
     def __init__(self, sender: str, with_date: bool, message: str) -> None:
         all_contracts = APP_CONTACTS
         super().__init__(sender, all_contracts, with_date, message)
 
-    def get_mail_subject(self):
+    def get_mail_subject(self) -> str:
         return f'{self.sender} sent a message to everyone via VoiceTranslator!'
+
+    @staticmethod
+    def get_correct_object(sender: str, text: str):
+        #fixme here
+        try:
+            pass
+        except:
+            return None
+
+
+# send file {filename} to {contact} with date
+class FileMail(Mail):
+    file_path: str
+
+    def __init__(self, sender: str, contacts: tuple, with_date: bool, file_path: str) -> None:
+        super().__init__(sender, contacts, with_date)
+        self.file_path = file_path
+
+    def __str__(self):
+        return f'{self.sender=} {self.contacts=} {self.with_date=} {self.file_path=}'
+
+    def get_mail_subject(self) -> str:
+        return f'{self.sender} sent you a file via VoiceTranslator!'
+
+    def get_mail_body(self) -> str:
+        return self.get_date_str_for_mail()
+
+    @staticmethod
+    def get_correct_object(sender: str, text: str):
+        #fixme here
+        try:
+            pass
+        except:
+            return None
+
+
+# send file {filename} to everybody with date
+class SharedFileMail(FileMail):
+    def __init__(self, sender: str, with_date: bool, file_path: str) -> None:
+        all_contracts = APP_CONTACTS
+        super().__init__(sender, all_contracts, with_date, file_path)
+
+    def get_mail_subject(self) -> str:
+        return f'{self.sender} sent a file to everyone via VoiceTranslator!'
+
+    @staticmethod
+    def get_correct_object(sender: str, text: str):
+        #fixme here
+        try:
+            pass
+        except:
+            return None
