c82067eb013acf4ac2e497a76bfb655c34a5db3f
Avital <avitalll16@gmail.com>
2022-05-25 18:15:29 +0300

    Open wav file and idtentify which type of email

--- Server/handlers.py
+++ Server/handlers.py
@@ -1,6 +1,7 @@
 def wav_file_handler(wav_path: str) -> str:
-    # TODO open wav file and convert it to text.
-    # TODO identify which type of Mail it is and create Mail object and send mail...
-    # This method is the core of the server
-
-    return 'recording content is returned'
+    text_from_voice = voice_to_text(wav_path)
+    print(text_from_voice)
+    sender = SENDER
+    mail_obj = mail_object_from_plain_text(sender, text_from_voice)
+    send_email(mail_obj)
+    return text_from_voice
