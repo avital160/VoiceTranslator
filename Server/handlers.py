11df89f046a924f1cbe69dc1fb122d4c67c63d64
Avital <avitalll16@gmail.com>
2022-05-25 18:29:15 +0300

    mail object from text Changes by Doriav Isakov

--- Server/handlers.py
+++ Server/handlers.py
@@ -1,3 +1,9 @@
+from secrets import SENDER
+from audio_actions import voice_to_text
+from mail_format import Mail, MessageMail, TranslatedMessageMail, SharedMessageMail, FileMail, SharedFileMail
+from email_sender import send_email
+
+
 def wav_file_handler(wav_path: str) -> str:
     text_from_voice = voice_to_text(wav_path)
     print(text_from_voice)
@@ -5,3 +11,14 @@ def wav_file_handler(wav_path: str) -> str:
     mail_obj = mail_object_from_plain_text(sender, text_from_voice)
     send_email(mail_obj)
     return text_from_voice
+
+
+def mail_object_from_plain_text(sender: str, text: str) -> Mail:
+    if text:
+        args = (sender, text)
+        return MessageMail.get_correct_object(*args) or \
+               TranslatedMessageMail.get_correct_object(*args) or \
+               SharedMessageMail.get_correct_object(*args) or \
+               FileMail.get_correct_object(*args) or \
+               SharedFileMail.get_correct_object(*args)
+
